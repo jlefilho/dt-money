11036d5ffc4d3202d6293947b54088fb838608f0
Jorge Espindola <jlespindolaf@gmail.com>
2022-10-06 16:05:05 -0300

    Base project stylization

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { ThemeProvider } from 'styled-components'
+import { defaultTheme } from './styles/themes/default'
+import { GlobalStyle } from './styles/global'
+import './App.css'
+
+export function App() {
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <h1>Hello world</h1>
+      <GlobalStyle />
+    </ThemeProvider>
+  )
+  
+}
+
+
