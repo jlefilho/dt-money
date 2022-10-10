bfd0fef7f5493bb65f0e55d3e47fd834443af1fd
Jorge Espindola <jlespindolaf@gmail.com>
2022-10-10 17:32:49 -0300

    Added search functionality

--- src/pages/Transactions/components/SearchForm/SearchForm.tsx
+++ src/pages/Transactions/components/SearchForm/SearchForm.tsx
@@ -4,6 +4,8 @@ import { zodResolver } from '@hookform/resolvers/zod'
 
 import { MagnifyingGlass } from "phosphor-react";
 import { SearchFormContainer } from "./styles";
+import { useContext } from "react";
+import { TransactionsContext } from "../../../../contexts/TransactionsContext";
 
 const searchFormSchema = z.object({
     query: z.string()
@@ -11,8 +13,9 @@ const searchFormSchema = z.object({
 
 type SearchFormInputs = z.infer<typeof searchFormSchema>
 
-
 export function SearchForm() {
+    const { fetchTransactions } = useContext(TransactionsContext)
+
     const { 
         register,
         handleSubmit,
@@ -22,8 +25,7 @@ export function SearchForm() {
     })
 
     async function handleSearchTransactions(data: SearchFormInputs) {
-        await new Promise (resolve => setTimeout(resolve, 2000))
-        console.log(data)
+        await fetchTransactions(data.query)
     }
 
     return (
